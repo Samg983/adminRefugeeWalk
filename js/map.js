0604a879fd688e7e86e7e9f126f8651f36a125bf
Sam <sam.goeman@hotmail.com>
2017-05-27 11:36:52 +0200

    map

---
+++ js/map.js
@@ -0,0 +1,286 @@
+
+var map;
+var database = firebase.database();
+var annotationNlCount;
+var iconBase = 'https://www.samgoeman.com/img/';
+var lat, lon;
+var icons = {
+    tales: {
+        icon: iconBase + 'tales.png'
+
+    },
+    eat: {
+        icon: iconBase + 'eat.png'
+    },
+    rock: {
+        icon: iconBase + 'rock.png'
+    }
+};
+$(document).ready(function () {
+    initMap();
+
+    readAllAnnotationsNl();
+
+    
+
+    
+    $("#submit").click(function () {
+        //alert(annotationNlCount);
+        var titel = $("#title").val();
+        var beschrijving = $("#beschrijving").val();
+        var categorie = $("#categorie").val();
+
+        writeAnnotationToFirebase(categorie, lat, lon, titel, beschrijving);
+    });
+});
+
+
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var styledMapType = new google.maps.StyledMapType([
+        {
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#f5f5f5"
+                }
+            ]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#616161"
+                }
+            ]
+        },
+        {
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#f5f5f5"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#bdbdbd"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#eeeeee"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#757575"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#e5e5e5"
+                }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#757575"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#dadada"
+                }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#616161"
+                }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+            ]
+        },
+        {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#e5e5e5"
+                }
+            ]
+        },
+        {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#eeeeee"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "color": "#c9c9c9"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "color": "#00b7b3"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+            ]
+        }
+    ], {name: 'Styled Map'});
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 50.977878, lng: 5.628322},
+        zoom: 11,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                'styled_map']
+        }
+    });
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+    map.addListener('click', function (e) {
+        $('#addMarkerPopUp').popup('toggle');
+        lat = e.latLng.lat();
+        lon = e.latLng.lng();
+        placeMarker(e.latLng, "eat", map);
+    });
+}
+
+function placeMarker(position, categorie, title, map) {
+    var marker = new google.maps.Marker({
+        draggable: true,
+        position: position,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: icons[categorie].icon
+    });
+    marker.addListener('click', toggleBounce);
+    google.maps.event.addListener(marker, 'dragend', function (e) {
+        console.log(e.latLng.lat());
+        console.log(marker.position);
+    });
+    map.panTo(position);
+}
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+function writeAnnotationToFirebase(categorie, lat, lon, titel, beschrijving) {
+    var annotationId = annotationNlCount;
+    //alert("annotationid: " + annotationId);
+    database.ref('nl/annotations/annotation' + annotationId).set({
+        categorie: categorie,
+        lat: lat,
+        lon: lon,
+        titel: titel,
+        beschrijving: beschrijving
+    });
+}
+
+var allAnnotations;
+function readAllAnnotationsNl() {
+    var annotationsRefNl = firebase.database().ref('nl/annotations');
+    annotationsRefNl.on('value', function (snapshot) {
+        allAnnotations = snapshot.val();
+        allAnnotationsKeys = Object.keys(allAnnotations);
+        annotationNlCount = allAnnotationsKeys.length;
+        for (var i = 0; i < allAnnotationsKeys.length; i++) {
+            var k = allAnnotationsKeys[i];
+
+            var categorie = allAnnotations[k].categorie;
+            var lat = allAnnotations[k].lat;
+            var lon = allAnnotations[k].lon;
+            var titel = allAnnotations[k].titel;
+            var beschrijving = allAnnotations[k].beschrijving;
+
+            var myLatlng = new google.maps.LatLng(lat, lon);
+            placeMarker(myLatlng, categorie, title, map);
+        }
+    });
+}
+
+
--- map.php
+++ map.php
@@ -11,28 +11,77 @@ include_once 'check_login.php';
     <body>
         <?php require_once 'nav.php'; ?>
         <main>
-            <a id="mapIcon" href="#modal1" class="btn-floating btn-large waves-effect waves-light teal tooltipped" data-position="left" data-delay="50" data-tooltip="Set new markers"><i class="material-icons">edit</i></a>
+            <div class="fixed-action-btn">
+                <a id="mapIcon" class="btn-floating btn-large teal">
+                    <i class="large material-icons">mode_edit</i>
+                </a>
+                <ul>
+                    <li><a class="btn-floating red"><i class="material-icons">delete</i></a></li>
+                </ul>
+            </div>
             <div id="map">
 
 
             </div>
 
-        
+            <div id="addMarkerPopUp" style="width:30%">
 
-            <!-- Modal Structure -->
-            <div id="modal1" class="modal bottom-sheet">
-                <div class="modal-content">
-                    <h4>Modal Header</h4>
-                    <p>A bunch of text</p>
-                </div>
-                <div class="modal-footer">
-                    <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-                </div>
+                <form class="s12" action="map.php">
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <select class="icons" name="categorie" id="categorie">
+                                <option value="" disabled selected>Kies een categorie</option>
+                                <option value="eat" data-icon="img/eat.png" class="left circle">Eat</option>
+                                <option value="rock" data-icon="img/rock.png" class="left circle">Rock</option>
+                                <option value="tales" data-icon="img/tales.png" class="left circle">Tales</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input placeholder="" id="title" type="text" class="validate" name="title">
+                            <label for="title">Titel</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea id="beschrijving" class="materialize-textarea" placeholder="" name="description"></textarea>
+                            <label for="beschrijving">Beschrijving</label>
+                        </div>
+                    </div>
+                    <button id="submit" class="btn waves-effect waves-light full-width" type="submit">Submit
+                        <i class="material-icons right">send</i>
+                    </button>
+                </form>
+
+
             </div>
+
+
+
+
+
         </main>
         <?php require_once './Footer.php'; ?>
         <script async defer
                 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDx-gTNGSJy30dmkvQvAjj3iS6y9vs8j7g&callback=initMap">
+        </script>
+        <script src="https://cdn.rawgit.com/vast-engineering/jquery-popup-overlay/1.7.13/jquery.popupoverlay.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/4.0.0/firebase.js"></script>
+        <script>
+                    // Initialize Firebase
+                    var config = {
+                        apiKey: "AIzaSyAxfL-FkIsfn_U0lC6ZyVbSoNeOONihbjU",
+                        authDomain: "refugeewalk-2bcad.firebaseapp.com",
+                        databaseURL: "https://refugeewalk-2bcad.firebaseio.com",
+                        projectId: "refugeewalk-2bcad",
+                        storageBucket: "refugeewalk-2bcad.appspot.com",
+                        messagingSenderId: "852551580647"
+                    };
+                    firebase.initializeApp(config);
+        </script>
+        <script
+            src="js/map.js">
         </script>
     </body>
 </html>
